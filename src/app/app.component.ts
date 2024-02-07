679b64bcf2cdc410bebbaac3e1bc8486706c6c9e
n <2185802@cegepmontpetit.ca>
2024-02-07 11:07:52 -0500

    FCT Fonctionnalité du projet
    Les deux requêtes sont maintenant fonctionnelles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,38 +8,43 @@ import { Album } from 'src/models/Album';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   constructor(public http : HttpClient){}
 
+  albums : Album[] = []
+
   album ?: Album;
+
+  chosenAlbum ?: Album;
 
   search ?: string;
 
   songs : string[] = [];
-
-  artists ?: string;
   
-  async request() : Promise<void>{
-    let x = await lastValueFrom(this.http.get<any>("https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" 
+  async searchAlbums() : Promise<void>{
+    this.albums = [];
+
+    //Get top albums from searched celebritie
+    let tempAlbums = await lastValueFrom(this.http.get<any>("https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" 
     +  this.search + "&api_key=9a8a3facebbccaf363bb9fd68fa37abf&format=json"));
 
-    console.log(x);
+    for (let i = 0; i < tempAlbums.topalbums.album.length - 40; i++) {
+      let tempAlbum = await lastValueFrom(this.http.get<any>("https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=9a8a3facebbccaf363bb9fd68fa37abf&artist="
+       +  this.search + "&album=" + tempAlbums.topalbums.album[i].name + "&format=json"));
 
-    for (let i = 0; i < x.topalbums.album.length; i++) {
-      let y = await lastValueFrom(this.http.get<any>("https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=9a8a3facebbccaf363bb9fd68fa37abf&artist="
-       +  this.search + "&album=" + x.topalbums.album[i].name + "&format=json"));
-       console.log(y);
+      this.songs = [];
+      for (let j = 0; j < tempAlbum.album.tracks.track.length; j++) {
+        this.songs.push(tempAlbum.album.tracks.track[j].name);
+      }
+
+      this.album = new Album(tempAlbum.album.artist,tempAlbum.album.name,tempAlbum.album.image[2]["#text"],this.songs);
+      this.albums.push(this.album);
     }
   }
 
-  async ngOnInit() : Promise<void>{
-    let x = await lastValueFrom(this.http.get<any>("https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=9a8a3facebbccaf363bb9fd68fa37abf&artist=Cher&album=Believe&format=json"))
-    console.log(x);
-    for (let i = 0; i < x.album.tracks.track.length; i++) {
-      this.songs.push(x.album.tracks.track[i].name);
-    }
-    console.log(this.songs);
-    this.album = new Album(x.album.artist,x.album.name,x.album.image[2]["#text"],this.songs);
+  clickAlbum(a : Album) {
+    console.log(a);
+    this.chosenAlbum = a;
   }
 }
 
